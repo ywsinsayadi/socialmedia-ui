4862d74dc92225cb9a9b6223afe86e938cbe6a64
yasin <95072831+ywsinsayadi@users.noreply.github.com>
2022-09-20 14:36:08 +0430

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\P.Andishe\\AppData\\Local\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "E:\\social\\social_media" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\P.Andishe\\AppData\\Local\\flutter"
+  "PROJECT_DIR=E:\\social\\social_media"
+  "FLUTTER_ROOT=C:\\Users\\P.Andishe\\AppData\\Local\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=E:\\social\\social_media\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=E:\\social\\social_media"
+  "FLUTTER_TARGET=E:\\social\\social_media\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=E:\\social\\social_media\\.dart_tool\\package_config.json"
+)
